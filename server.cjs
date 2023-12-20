1c60f9beca03df143ebf869a77a8f05e694d4a12
barrettxavier <barrettxavier@yahoo.com>
2023-12-20 16:33:40 -0500

    updated sever.js with PORT

--- server.cjs
+++ server.cjs
@@ -1,16 +1,16 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static(__dirname + "./dist/assets"));
 
-app.get("/", (req, res) => res.send(__dirname + "/dist/index.html"));
+app.get("/", (req, res) => res.send(__dirname + "./dist/index.html"));
 
 app.use("/api", require("./api/index.cjs"));
 
 app.get("/", (req, res) => res.send("Splice Of Life!"));
 
-app.listen(port, () => console.log(`Server listening on port: ${port}!`));
+app.listen(PORT, () => console.log(`Server listening on port: ${PORT}!`));
