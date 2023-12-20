5faff31ffb9cea873111146945a2ec296d50285f
jpickler <sirzymok@gmail.com>
2023-12-20 12:56:54 -0500

    Home page and other pages with routes created. Most are placeholder files.

---
+++ src/pages/registration.jsx
@@ -0,0 +1,36 @@
+
+
+function RegistrationPage() {
+ 
+
+  return (
+
+<> 
+
+<header>
+    <h1> Welcome to Splice Of Life</h1>
+    <nav>
+      <button id="accountButton"> Account</button>
+      <button id="registerButton"> Register</button>
+      <button id="loginButton"> Login </button>
+      <button id="cartButton"> Cart </button>
+    </nav>
+</header>
+
+<body> 
+
+<h1 class="heading"> Registration Page</h1>
+ 
+</body>
+
+
+<footer>
+  <h1> We sell the best mutated creatures known to man - Splice Of Life</h1>
+</footer>
+
+</>
+
+  );
+};
+
+export default   RegistrationPage;
