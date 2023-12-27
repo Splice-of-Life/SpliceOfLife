8e5f91285425ccd4ada2122478967f4434a0766e
barrettxavier <barrettxavier@yahoo.com>
2023-12-27 16:46:54 -0500

    added a lot more to css

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,11 @@
 import { Link } from "react-router-dom";
 import Image from "../assets/images/SpliceOfLife_Logo.png";
+import { ShoppingCartIcon } from "lucide-react";
 
 const NavBar = () => {
   return (
-    <div className="flex justify-between py-10 px-8 ">
+    <div className="flex justify-between py-10 px-20 bg-white text-black">
+      {/* Left side holds Registration, Account, and Login */}
       <nav>
         <ul className="flex gap-6 text-lg items-center h-full">
           <li className="btn">
@@ -17,27 +19,32 @@ const NavBar = () => {
           </li>
         </ul>
       </nav>
-      <Link className="flex gap-6 items-center" to="/">
-        {/* <img
-          className="w-10 h-10 rounded-full"
-          src="https://media.giphy.com/media/3o85xGrC7nPVbA2y3K/giphy.gif"
-          alt="dna strands being exchanged"
-        /> */}
-        {/* <h1 className="text-4xl font-bold">S / L</h1> */}
+
+      {/* Spice of life logo img ================================================ */}
+      <Link className="flex gap-6 items-center flex-shrink-0" to="/">
         <img className="w-[12rem]" src={Image} alt="Splice Of Life Logo" />
       </Link>
-      <nav>
 
+      {/* Right side holds Home, Search, and Cart */}
+      <nav>
         <ul className="flex gap-6 text-lg items-center h-full">
-          <li className="btn"
+          <li className="btn">
             <a href="/">Home</a>
           </li>
           <li className="btn">
             <a href="#">Search</a>
           </li>
-          <li className="btn">
-            <a href="/cart">Cart</a>
-          </li>
+          <Link
+            className="ml-6 hover:bg-slate-100 px-6 py-1 rounded-lg hover:shadow-md"
+            to="/cart"
+          >
+            {/* Cart icon with bouncing ping ========================================= */}
+            <span className="relative flex ml-6 h-3 w-3">
+              <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-sky-400 opacity-75"></span>
+              <span className="relative inline-flex rounded-full h-3 w-3 bg-sky-500"></span>
+            </span>
+            <ShoppingCartIcon />
+          </Link>
         </ul>
       </nav>
     </div>
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,5 +1,11 @@
 import { Link } from "react-router-dom";
 import { useEffect, useState } from "react";
+import Image from "../assets/images/DNA_wallpaper.jpg";
+import {
+  ArrowBigDownIcon,
+  ArrowDownCircleIcon,
+  ArrowDownNarrowWide,
+} from "lucide-react";
 
 function HomePage() {
   // need to render cards with data once db is set up
@@ -23,10 +29,44 @@ function HomePage() {
   return (
     <>
       <div>
-        <h1> Our Animals</h1>
+        {/* Hero section ================================================ */}
+        <section className="absolute z-20 flex ">
+          <div>
+            <h1 className="text-white -rotate-90 text-1xl font-bold mt-[40vh]">
+              A LITTLE BIT OF THIS, A LITTLE BIT OF THAT
+            </h1>
+          </div>
+          <div className=" w-[80vw]">
+            <h1 className="w-[60rem] text-white text-8xl mt-[200px]">
+              <span className="bg-white text-black p-4 font-semibold">
+                Leading
+              </span>
+              <span> the way</span>
+              <br />
+              <span className="leading-relaxed">in genetic Mutations</span>
+              <hr className="w-[40vw] border-dashed mt-6 opacity-40" />
+            </h1>
+          </div>
+        </section>
+
+        {/* Down arrow icon ================================================ */}
+        <div className="flex justify-center text-white absolute bottom-0 w-screen mb-10 animate-bounce">
+          <div className="flex flex-col items-center">
+            <p className="mb-4 text-lg">Lets Take a Look</p>
+            <ArrowDownCircleIcon />
+          </div>
+        </div>
+
+        {/* background image ================================================ */}
+        <img
+          className="w-screen h-screen opacity-40"
+          src={Image}
+          alt="DNA strands"
+        />
+
+        {/* Grid layout for creatures ================================================ */}
         <div className="grid w-screen mx-0 grid-cols-1 laptop:grid-cols-2 laptop:grid-rows-3 desktop:grid-cols-3">
           {/* this will map from the database to fill breed and image */}
-
           {creatures.map((creature) => (
             <Link key={creature.id} to={`creatures/${creature.id}`}>
               <div className="card-primary group relative overflow-hidden">
